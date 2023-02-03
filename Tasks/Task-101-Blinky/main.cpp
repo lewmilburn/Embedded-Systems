f75120b1bd0e5a41b16df6b83f9ee6c453f6cd13
Lewis Milburn <lewiscmilburn@gmail.com>
2023-02-03 11:24:37 +0000

    Completed blinky task

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -1,12 +1,12 @@
+// Task 101 Blinky - Lewis Milburn
+// [COMPLETED] 03/02/2023
+
 #include "mbed.h"
 
-// Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
+// LED objects
 DigitalOut greenLED(LED1);
-
-//These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
-//Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
@@ -16,16 +16,20 @@ int main()
     { 
         //The code between the { curly braces } is the code that is repeated forever
 
-        // Turn onboard LED ON  
+        // Sequence 1
+        redLED = 1; 
         greenLED = 1; 
+        blueLED = 0;
 
         // Wait 0.2 second (1 million microseconds)
-        wait_us(100000); 
+        wait_us(500000); 
 
         // Turn LED OFF
-        greenLED = 0;
+        redLED = 0; 
+        greenLED = 0; 
+        blueLED = 1;
 
         // Wait 0.2 second
-        wait_us(100000); 
+        wait_us(500000); 
     }
 }
